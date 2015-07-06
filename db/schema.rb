8bc11e3232f8d92216f818037cd6d1db7cbd5ac7
Adil Patel <Adil@Adils-MacBook-Pro.local>
2015-07-06 12:34:19 -0500

    High starts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622165432) do
+ActiveRecord::Schema.define(version: 20150622170813) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
